85cdb262f1fecf0ae8d3808af01719f13b3ec75c
Vishva Patel <88160041+vishva7049@users.noreply.github.com>
2022-02-23 15:54:35 -0500

    added class files.

---
+++ ApplicantEduactionPoco.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table ("Applicant_Educations")]
+    public class ApplicantEducationPoco : IPoco
+    {
+        [Key]  
+        public Guid Id { get; set; }
+        public Guid Applicant { get; set; }
+        public String Major { get; set; }
+
+        [Column("Certificate_Diploma")]
+        public string CertificateDiploma { get; set; }
+
+        [Column("Start_Date")]
+        public DateTime? StartDate { get; set; }
+
+        [Column("Completion_Date")]
+        public DateTime? CompletionDate { get; set; }
+
+        [Column("Completion_Percent")]
+        public Byte? CompletionPercent { get; set; }
+
+        [Column("Time_Stamp")]
+        public byte [] TimeStamp { get; set; }
+
+
+    }
+}
---
+++ IPoco.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.Pocos
+{
+    public interface IPoco
+    {
+        public Guid Id { get; set; }
+    
+    }
+}
